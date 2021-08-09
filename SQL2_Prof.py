3dd67c68d201b1d3908a50b9d89820553b97b4d7
totosmurra <82951283+totosmurra@users.noreply.github.com>
2021-08-09 19:09:26 -0300

    .

--- SQL2_Prof.py
+++ SQL2_Prof.py
@@ -17,7 +17,7 @@ def create_schema():
 
     c.execute("""
             CREATE TABLE DatosMendoza(
-                [id] INTEGER PRIMARY KEY,
+                [id] TEXT PRIMARY KEY,
                 [site_id] TEXT,
                 [title] TEXT,
                 [price] INTEGER,
@@ -67,48 +67,51 @@ def fill():
 
     for i in lista_con_ids:
         
-        with open ("MELI_SQL_ORM", "w") as jsonfile:
-            URL = "https://api.mercadolibre.com/items?ids="+i
-            response = requests.get(URL)
-            datos1 = response.json()
-            json.dump(datos1, jsonfile, indent = 4)
-            datos = datos1[0]["body"]
+        
+        URL = "https://api.mercadolibre.com/items?ids="+i
+        response = requests.get(URL).json()
+        datos = response[0]["body"]
 
-        try:
+
             
-            for x in datos:
-                
+        for asd in datos:
+                    
                 #datos_utiles = {"id":x["id"], "site_id":x["site_id"], "title":x["title"], "price":x["price"], "currency_id":x["currency_id"], "initial_quantity":x["initial_quantity"], "available_quantity":x["available_quantity"], "sold_quantity":x["sold_quantity"]}
                 #datos_utiles = x["id"], x["site_id"], x["title"], x["price"], x["currency_id"], x["initial_quantity"], x["available_quantity"], x["sold_quantity"]
-                
-                id = x["id"]
-                site_id = x["site_id"]
-                title = x["title"]
-                price = x["price"]
-                currency_id = x["currency_id"]
-                initial_quantity = x["initial_quantity"]
-                available_quantity = x["available_quantity"]
-                sold_quantity = x["sold_quantity"]
+            #try:        
+                id1 = (datos["id"])
+                site_id1 = (datos["site_id"])
+                title1 = (datos["title"])
+                price1 = (datos["price"])
+                currency_id1 = (datos["currency_id"])
+                initial_quantity1 = (datos["initial_quantity"])
+                available_quantity1 = (datos["available_quantity"])
+                sold_quantity1 = (datos["sold_quantity"])
 
-                dataset = [(id, site_id, title, price, currency_id, initial_quantity, available_quantity, sold_quantity)]
+                #dataset = (id, site_id, title, price, currency_id, initial_quantity, available_quantity, sold_quantity)
 
                 #dataset = [{i["id"]}, {i["site_id"]}, {i["title"]}, {i["price"]}, {i["currency_id"]}, {i["initial_quantity"]}, {i["available_quantity"]}, {i["sold_quantity"]}]
 
-                
-                
+                dataset = [id1],[site_id1],[title1],[price1],[currency_id1],[initial_quantity1],[available_quantity1],[sold_quantity1]
+                    
                 conn = sqlite3.connect('MELI.db')
 
                 c = conn.cursor()
 
-                c.executemany   ("""INSERT INTO DatosMendoza (id, site_id, title, price, currency_id, initial_quantity, available_quantity, sold_quantity)
-                                VALUES (?,?,?,?,?,?,?,?);""", dataset)
+                
+                c.executemany   ("""INSERT INTO DatosMendoza id, site_id, title,price, currency_id, initial_quantity, available_quantity, sold_quantity 
+                                VALUES (?,?,?,?,?,?,?,?)""", dataset)
 
                 conn.commit()
+            
+            #except:
+                #continue
+        
+
 
         
         
-        except:
-            continue
+        
         
     print ("Agarre de datos importantes finalizado")
 
